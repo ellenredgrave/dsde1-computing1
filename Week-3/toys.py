05fb62c11a5ae5a704f4f5a49f064934972f718f
ellenredgrave <ellen.redgrave19@imperial.ac.uk>
2019-10-17 15:01:17 +0100

    update

--- Week-3/toys.py
+++ Week-3/toys.py
@@ -1,6 +1,5 @@
 '''
 toys.py
-
 Simple toy functions to get comfortable working 
 with functions.
 '''
@@ -9,20 +8,20 @@ with functions.
 # write a function that adds 1
 # to the input and prints the result
 def inc(a):
-    print(a)
-
+    total = (a + 1)
+    print(total)
 
 # write a function that adds 1
 # to the input and returns the result
 def inc_return(a):
-    return # hint this is incomplete
-
+    return a+1 # hint this is incomplete
 
 # write a function that adds
 # the two input numbers together
 # and returns the sum
 def sum(a, b):
-    return
+    total = a+b
+    return total
 
 
 # write a function that takes two
@@ -30,14 +29,18 @@ def sum(a, b):
 # sum() and then increments the sum
 # using inc_return
 def sum_inc(a, b):
-    return
+    banana = sum(a,b)
+    inc_return(banana + 1)
 
 
 # write a function that returns a 
 # boolean (True or False) for whether 
 # the input number is even
 def is_even(a):
-    return
+    if (a % 2) == 0:
+        return True
+    else:
+        return False
 
 
 # create for loop that takes a string
@@ -50,4 +53,3 @@ def string_repeat(phrase, repeat):
     # hint: you can add strings together 
     # in order to concatenate them
     return
-
